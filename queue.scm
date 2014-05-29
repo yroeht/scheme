5982f6986b33a0e39d322d8146f211a81816eb56
Theophile Ranquet <ranquet@lrde.epita.fr>
2014-05-29 18:06:46 +0200

    queue: stylistic changes
      * queue.scm: here, reindent.

--- queue.scm
+++ queue.scm
@@ -2,13 +2,13 @@
   (let ((front '()) (back '()))
     (lambda (cmd . element)
       (case cmd
-      ('push (set! back (cons (car element) back)))
-      ('peek (car back))
-      ('empty (and (null? front) (null? back)))
-      ('pop (begin (cond ((null? front)
-                          (set! front (reverse back)) (set! back '())))
-                   (let ((e (car front))) (set! front (cdr front)) e)))
-      ('dump (begin (display front) (display (reverse back))))))))
+        ('push (set! back (cons (car element) back)))
+        ('peek (car back))
+        ('empty (and (null? front) (null? back)))
+        ('pop (begin (cond ((null? front)
+                            (set! front (reverse back)) (set! back '())))
+                     (let ((e (car front))) (set! front (cdr front)) e)))
+        ('dump (begin (display front) (display (reverse back))))))))
 
 (define q (make-queue))
 
